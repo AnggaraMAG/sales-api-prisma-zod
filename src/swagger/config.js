880012638d426efc3a11b6326328bffd85f52f02
AnggaraMAG <anggaramag.dev@gmail.com>
2024-12-14 22:34:30 +0700

    .

--- src/swagger/config.js
+++ src/swagger/config.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.API_URL || 'http://localhost:3000',
+        url: process.env.API_URL,
         description: 'API Server',
       }
     ],
